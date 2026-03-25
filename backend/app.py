d243a5fc5950cb9ee33b015ddfa94551979408ba
aishwaryaa-prakash <aishwaryaaprakash88@gmail.com>
2026-03-25 21:56:03 +0530

    added voice input

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import re
 
 app = Flask(__name__)
+CORS(app)
 
 # Load ML model
 model = pickle.load(open("../ml_model/model.pkl", "rb"))
